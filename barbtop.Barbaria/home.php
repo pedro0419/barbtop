99c837d27ebc39e214c640271723dd0356801815
pedro0419 <168198127+pedro0419@users.noreply.github.com>
2025-06-01 15:12:21 -0300

    Add files via upload

---
+++ barbtop.Barbaria/home.php
@@ -0,0 +1,19 @@
+<?php
+        include("protecao.php")
+    ?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Bem-vindo!!!</h1>
+    <form action="logout.php" method = "post">
+        <input type="submit" value = "sair">
+    </form>
+</body>
+</html>
